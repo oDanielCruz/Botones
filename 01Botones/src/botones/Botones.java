30e196f6245cc5dd546187e18e2157870746525b
thelegendkiller <52903659+thelegendkiller@users.noreply.github.com>
2020-10-20 19:15:12 -0500

    Se le agregaron comentarios para explicar el funcionamiento del codigo.

--- 01Botones/src/botones/Botones.java
+++ 01Botones/src/botones/Botones.java
@@ -8,16 +8,18 @@ import javax.swing.JFrame;
 public class Botones {
 
   public static void main(String[] args) {
+    // Se crea un objeto de la clase PanelBotones
     PanelBotones panel = new PanelBotones();                      // Vista
+    // Se crea un objeto de la clase OyenteBoton
     OyenteBoton oyente = new OyenteBoton(panel);                  // Controlador
     panel.addEventos(oyente);                                     // Receptor
     
-    JFrame f = new JFrame("Ventana Botones");
-    f.setSize(800, 600);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.setLocationRelativeTo(f);
+    JFrame f = new JFrame("Ventana Botones"); // Creo un objeto de la clase JFrame
+    f.setSize(800, 600); // Se le define un ancho y un alto a la ventana.
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Le agregamos propiedades al boton x (o cerrar)
+    f.setLocationRelativeTo(f); // Centramos la ventana
     f.setContentPane(panel);
-    f.setVisible(true);
+    f.setVisible(true); // Hacemos visible la ventana
   }
   
 }
